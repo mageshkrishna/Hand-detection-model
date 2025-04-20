d0e9bd33afab629a1b32bf311222a1e1d1165c21
mageshkrishna <gmageshkrishna2003@gmail.com>
2025-04-21 00:01:34 +0530

    Reader Writer Done!

--- Core/IO/Reader.cpp
+++
@@ -1,8 +0,0 @@
-#include "Reader.h"
-#include "../Image.h"
-#include <iostream>
-
-void Reader::print(){
-    Image image(1 , 2);
-    std::cout << "This is from reader" << std::endl;
-}
--- main.cpp
+++ main.cpp
@@ -1,11 +1,40 @@
 #include <iostream>
-#include "Core/IO/Reader.h"
-#include "Core/Image.h"
+#include "core/Image.h"
 
 int main() {
     std::cout << "The very first program for the Hand-detection model project" << std::endl;
-    Reader read;
-    read.print();
+    
+    // Create an imaging namespace alias for convenience
+    using namespace imaging;
+    
+    // Create an image
+    Image myImage;
+    
+    // Load an image from file
+    if (myImage.loadFromBMP("/home/mageshkrishna/code-files/Hand Detection model from scrath using C++/Hand-detection-model/sample.bmp")) {
+        std::cout << "Successfully loaded image with dimensions: " 
+                  << myImage.getWidth() << "x" << myImage.getHeight() << std::endl;
+        
+        // Example: Process the image (inverting colors)
+        for (unsigned int y = 0; y < myImage.getHeight(); ++y) {
+            for (unsigned int x = 0; x < myImage.getWidth(); ++x) {
+                Pixel& pixel = myImage.getPixel(x, y);
+                // Invert the pixel colors
+                pixel.r = 255 - pixel.r;
+                pixel.g = 255 - pixel.g;
+                pixel.b = 255 - pixel.b;
+            }
+        }
+        
+        // Save the processed image
+        if (myImage.saveToBMP("output.bmp")) {
+            std::cout << "Successfully saved processed image" << std::endl;
+        } else {
+            std::cerr << "Failed to save processed image" << std::endl;
+        }
+    } else {
+        std::cerr << "Failed to load image" << std::endl;
+    }
+    
     return 0;
 }
-
